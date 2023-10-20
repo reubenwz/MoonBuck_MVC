dc44fc76455f5a86cf2f07b3e9c52f4f52c2be0c
Reuben <reubenangwz@gmail.com>
2023-10-20 18:58:51 +0800

    Add project files.

---
+++ MoonBuck/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoonBuck.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
